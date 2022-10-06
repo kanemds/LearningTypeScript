f4e5d8f4bbce206a346e05b6be9e25a941990709
Kanem <kanemdeng@email.com>
2022-10-06 03:41:36 -0700

    leant how to set from ts root folder to compile folder location

---
+++ lesson6-organizeFile/public/test.js
@@ -0,0 +1,20 @@
+"use strict";
+// 1 in terminal type:
+// tsc--init
+// ts.json file will show up
+// 2. 
+// find and change to
+//   "rootDirs": "./src",
+// 3. 
+// "outDir": "./public",
+//4. 
+// at the very bottom add :
+// ,
+//   "include": [
+//     "src"
+//   ]
+// 1 create .json
+// 2 set ts root file 
+// 3 set store compile file path
+// 4 set only compile tx files in folder
+let age = 35;
