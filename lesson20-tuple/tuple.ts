88c1212a3d530780576e7d632e6fad43942cd713
Kanem <kanemdeng@email.com>
2022-10-08 18:21:44 -0700

    learnt tuple

---
+++ lesson20-tuple/tuple.ts
@@ -0,0 +1,19 @@
+
+
+
+valuesOne = ["abc", 123, false]
+
+// ...valuesOne need to be verify first
+const newValue = [...valuesOne]
+
+console.log(newValue)
+
+/////////////////////////////////////////////////////////////
+
+let valuesTwo: [string, number, boolean] // <--- verify 
+
+valuesTwo = ["abc", 123, false]
+
+const newValueTwo = [...valuesTwo]
+
+console.log(newValue)
